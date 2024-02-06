1fe5d529be1f2e4854766326eab3ac7b665336bb
Paul Bagnicki <paulbagnicki@googlemail.com>
2024-02-06 20:19:41 +0000

    updated to remove alt for image.

--- obj/Debug/net8.0/MindBodySynergy.AssemblyInfo.cs
+++ obj/Debug/net8.0/MindBodySynergy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindBodySynergy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6866d234f2f170e1ccb370552fc234f3d66f9b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa1254129833682ff552f4afeae54aee819ea04")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindBodySynergy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindBodySynergy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
